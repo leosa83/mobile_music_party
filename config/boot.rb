d60fa237791088318f4bd7490267eac6019a0ffe
root <root@shaun-Studio-XPS-1645.(none)>
2011-10-27 01:47:22 -0700

    update

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,14 @@
+Gem.clear_paths
 require 'rubygems'
-
+gem 'bundler' #koz added http://stackoverflow.com/questions/2282969/rails3-server-and-bundler-error-uninitialized-constant-bundler-nameerror
 # Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+gemfile = File.expand_path('../../Gemfile', __FILE__)
+begin
+  ENV['BUNDLE_GEMFILE'] = gemfile
+  require 'bundler'
+  Bundler.setup
+rescue Bundler::GemNotFound => e
+  STDERR.puts e.message
+  STDERR.puts "Try running `bundle install`."
+  exit!
+end if File.exist?(gemfile)
